088b70be472c2022a73ee23393d1c9abfbddff0b
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-27 12:54:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog
-  def name
-    @name= name
+  def initialize(name,breed)
+    @name = name
+    @breed = breed
   end
 
   def name
     @name
+  end
+  def breed
+    @breed
   end
 
 end
